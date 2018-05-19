b71d456be2fe530bad838f5a568ac8f595d9b5f6
data-pup <kevinmartinucr@gmail.com>
2018-05-19 08:32:45 -0700

    Yay! Simple example is working now.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 extern crate regex;
 
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+    // Create a regex using a string, note that this is a result.
+    let r = regex::Regex::from_str("hell*").unwrap();
+
+    // Declare an array of items that we will search through for matches.
+    let data = &["why", "hello", "hell", "world"];
+
+    // Iterate through the array, print any items that match the regex.
+    data.iter()
+        .filter(|item| r.is_match(item))
+        .for_each(|item| println!("{}", item));
 }
